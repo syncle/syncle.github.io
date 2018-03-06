6677c3cb0a47633fd203614ff556899de5d6f954
Jaesik Park <syncle@gmail.com>
2018-03-06 01:25:54 -0800

    fancier spacing

--- script/gen_publication_list.py
+++ script/gen_publication_list.py
@@ -7,19 +7,20 @@ def write_html(file, data):
 	f.write('<div class="container"> <!-- begin of publication list -->\n\n')
 	for data_iter in data:
 		if data_iter.special_year != -1:
-			f.write('<div class="row">\n'+
+			f.write('<div class="container">\n'+
+					'<div class="row">\n'+
 					'	<div class="col-lg-12 text-left">\n'+
 					'		<h2 class="name">%s</h2>\n' % data_iter.special_year+
 					'	</div>\n'+
+					'</div>\n'+
 					'</div>\n\n')
 		else:
 			button = get_button_str_all(data_iter)
-			f.write('<div class="row">\n'+
+			f.write('<div class="container">\n'+
+					'<div class="row">\n'+
 					# Image
 					'	<div class="col-lg-4 col-lg-offset-1">\n'+
 					'		<img src="%s" style="max-height:170px" class="img-responsive center-block img-thumbnail">\n' % data_iter.image +
-					# Some stupid spacing
-					'		<p><br></p>\n'+
 					'	</div>\n'+
 					# Paper information
 					'	<div class="col-lg-7">\n'+
@@ -30,6 +31,10 @@ def write_html(file, data):
 					# Buttons
 					button +
 					'	</div>\n'+
+					'</div>\n'+
+					# just for adding some space - not sure style="height" is compatible for browsers
+					'<div class="row" style="height: 30px">\n'+
+					'</div>\n'+
 					'</div>\n\n')
 	f.write('</div>  <!-- end of publication list -->\n\n')
 	f.close()
