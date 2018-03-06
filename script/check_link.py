5558203784944f3e5eaf97a29c79b5e06e96c620
Jaesik Park <syncle@gmail.com>
2018-03-06 12:15:12 -0800

    Python2/3 for check_link.py

--- script/check_link.py
+++ script/check_link.py
@@ -1,28 +1,39 @@
 
-# this script only for Python 3
-import urllib.request
-from urllib.parse import urlparse
-import os
+import os, sys
+if (sys.version_info > (3, 0)):
+	pversion = 3
+else:
+	pversion = 2
 
-ROOT_FOLDER = os.path.dirname(os.getcwd())
+if pversion == 3:
+	import urllib.request
+	from urllib.parse import urlparse
+elif pversion == 2:
+	import urllib2
+	from urlparse import urlparse
 
 def is_absolute(url):
 	return bool(urlparse(url).netloc)
 
 def check_url(link):
-	"""
-	Checks that a given URL is reachable.
-	:param url: A URL
-	:rtype: bool
-	"""
+	# only retrieve HEAD to save time.
 	if is_absolute(link):
-		try:
-			request = urllib.request.Request(link)
-			request.get_method = lambda: 'HEAD'
-			urllib.request.urlopen(request)
-		except urllib.request.HTTPError:
-			print('Warning:: broken link %s' % link)
+		if pversion == 3:
+			try:
+				request = urllib.request.Request(link)
+				request.get_method = lambda: 'HEAD'
+				urllib.request.urlopen(request)
+			except urllib.request.HTTPError:
+				print('Warning:: broken link %s' % link)
+		elif pversion == 2:
+			try:
+				request = urllib2.Request(link)
+				request.get_method = lambda: 'HEAD'
+				urllib2.urlopen(request)
+			except urllib2.HTTPError:
+				print('Warning:: broken link %s' % link)
 	else:
-		abs_path = os.path.join(ROOT_FOLDER, link)
+		root_folder = os.path.dirname(os.getcwd())
+		abs_path = os.path.join(root_folder, link)
 		if os.path.exists(abs_path) == False:
 			print('Warning:: broken link %s' % abs_path)
