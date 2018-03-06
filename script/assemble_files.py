7060c51c647cd9eae6e76b4c686fb5d05fb7f1c5
Jaesik Park <syncle@gmail.com>
2018-03-06 14:13:09 -0800

    polishing for project

--- script/assemble_files.py
+++ script/assemble_files.py
@@ -26,7 +26,7 @@ def gen_project_page(path, title):
 	include_file('../' + path + 'index.html', '../' + path + 'content_index.html')
 	record = read_record('../' + path + 'publication_data.txt')
 	#print(record[0])
-	append_button_html(path, 'index.html', record)
+	append_button_html('../' + path + 'index.html', record)
 	#include_file(path + '/index.html', '../source/publications_footer.html')
 	include_file('../' + path + 'index.html', path_footer)
 
