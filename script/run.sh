36c703308e8ff6e6366e2ce46d0275f5cc53715c
Jaesik Park <syncle@gmail.com>
2018-03-06 10:43:42 -0800

    horizontal line in publications and polishing script

--- script/run.sh
+++ script/run.sh
@@ -1,6 +1,19 @@
+#!/bin/sh
 
-python gen_publication_list.py
-gen_button_for_project_page.py
-python gen_headers.py
-python assemble_files.py
-python resize_images.py
+# bash function for checking return value of the script
+run_and_check_success()
+{
+	$1					# run script
+	success=$?			# store return value of the script
+	if [ $success -ne 0 ]; then
+	    echo "Job failed:: $1"
+	else
+		echo "Job success:: $1"
+	fi
+}
+
+run_and_check_success "python gen_publication_list.py"
+run_and_check_success "python gen_button_for_project_page.py"
+run_and_check_success "python gen_headers.py"
+run_and_check_success "python assemble_files.py"
+run_and_check_success "python resize_images.py"
