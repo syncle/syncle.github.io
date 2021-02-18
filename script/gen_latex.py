f54d7b6cbe5cab46a2209739765698aae362ebff
Jaesik Park <syncle@gmail.com>
2021-02-18 22:13:50 +0900

    wip

--- script/gen_latex.py
+++ script/gen_latex.py
@@ -4,21 +4,37 @@ from util import *
 
 def write_latex_publication(file, data):
 	f = open(file, "w")
-	f.write('\\section{Publications}\n')
+	f.write('\\section{Publications}\n\n\n\n')
+	f.write('International\n')
 	f.write('\\resumePublicationListStart\n')
-	# import ipdb; ipdb.set_trace()
+	# international
 	for year in data:
 		for category, items in data[year].items():
 			if category == 'papers':
 				for i in items:
 					if i['type'] == 'international':
-						f.write('\publicationItem' + \
+						f.write('    \publicationItem' + \
 						'{' + change_bold(authorlist_to_text(i['author'])) + '}' + \
 						'{' + i['title'] + '}' + \
 						'{' + change_bold(i['venue']) + '}\n' \
 						)
-	f.write('\\resumePublicationListEnd\n')
+	f.write('\\resumePublicationListEnd\n\n\n\n')
+	# domestic
+	f.write('Domestic\n')
+	f.write('\\resumePublicationListStart\n')
+	for year in data:
+		for category, items in data[year].items():
+			if category == 'papers':
+				for i in items:
+					if i['type'] == 'domestic':
+						f.write('    \publicationItem' + \
+						'{' + change_bold(authorlist_to_text(i['author'])) + '}' + \
+						'{' + i['title'] + '}' + \
+						'{' + change_bold(i['venue']) + '}\n' \
+						)
+	f.write('\\resumePublicationListEnd\n\n\n\n')
 	f.close()
+
 
 def write_latex_awards(file, data):
 	f = open(file, "w")
@@ -29,13 +45,86 @@ def write_latex_awards(file, data):
 			if category == 'awards':
 				for i in items:
 					if not 'hide' in i:
-						f.write('\\resumeItem' + \
+						f.write('    \\resumeItem' + \
 						'{' + i['title'] + ', ' \
 							+ i['awardee'] + ', ' \
 							+ (i['comment']  + ', ' if 'comment' in i else '') \
 							+ number_to_month(i['month']) + ' ' + year + '}\n')
-	f.write('\\resumeItemListEnd\n')
+	f.write('\\resumeItemListEnd\n\n\n\n')
 	f.close()
+
+
+def write_latex_patents(file, data):
+	f = open(file, "w")
+	f.write('\\section{Patents}\n')
+	f.write('\\resumeItemListStart\n')
+	for year in data:
+		for category, items in data[year].items():
+			if category == 'patents':
+				for i in items:
+					if not 'hide' in i:
+						f.write('    \\resumeItem' + \
+						'{' + i['title'] + ', ' \
+							# + i['awardee'] + ', ' \
+							+ (i['comment']  + ', ' if 'comment' in i else '') \
+							+ '}\n')
+	f.write('\\resumeItemListEnd\n\n\n\n')
+	f.close()
+
+
+def write_latex_talks(file, data):
+	f = open(file, "w")
+	f.write('\\section{Talks}\n')
+	f.write('\\resumeItemListStart\n')
+	for year in data:
+		for category, items in data[year].items():
+			if category == 'talks':
+				for i in items:
+					if not 'hide' in i:
+						f.write('    \\resumeItem' + \
+						'{' + i['title'] + ', ' \
+							+ i['venue'] + ', ' \
+							+ (i['comment']  + ', ' if 'comment' in i else '') \
+							+ number_to_month(i['month']) + ' ' + year + '}\n')
+	f.write('\\resumeItemListEnd\n\n\n\n')
+	f.close()
+
+
+def write_latex_teaching(file, data):
+	f = open(file, "w")
+	f.write('\\section{Teaching}\n')
+	f.write('\\resumeItemListStart\n')
+	for year in data:
+		for category, items in data[year].items():
+			if category == 'teaching':
+				for i in items:
+					if not 'hide' in i:
+						f.write('    \\resumeItem' + \
+						'{' + i['title'] + ', ' \
+							+ i['semester'] + ', ' \
+							+ (i['comment']  + ', ' if 'comment' in i else '') \
+							+ ' ' + year + '}\n')
+	f.write('\\resumeItemListEnd\n\n\n\n')
+	f.close()
+	
+
+def write_latex_program_committe(file, data):
+	f = open(file, "w")
+	f.write('\\section{Program Committee}\n')
+	f.write('\\resumeItemListStart\n')
+	for year in data:
+		for category, items in data[year].items():
+			if category == 'program committee':
+				for i in items:
+					if not 'hide' in i:
+						f.write('    \\resumeItem' + \
+						'{' + i['title'] + ', ' \
+							+ i['venue'] + ', ' \
+							+ (i['comment']  + ', ' if 'comment' in i else '') \
+							+ ' ' + year + '}\n')
+	f.write('\\resumeItemListEnd\n\n\n\n')
+	f.close()
+
 
 if __name__ == "__main__":
 	with open('../data/record.json') as f:
@@ -43,10 +132,18 @@ if __name__ == "__main__":
 	
 	write_latex_publication('../source/latex/publication.tex', data)
 	write_latex_awards('../source/latex/awards.tex', data)
+	write_latex_patents('../source/latex/patents.tex', data)
+	write_latex_teaching('../source/latex/teaching.tex', data)
+	write_latex_talks('../source/latex/talks.tex', data)
+	write_latex_program_committe('../source/latex/program_committe.tex', data)
 
 	page = 'cv'
 	clear_file('../%s.tex' % page)
 	include_file('../%s.tex' % page, '../source/latex/header.tex')
 	include_file('../%s.tex' % page, '../source/latex/publication.tex')
+	include_file('../%s.tex' % page, '../source/latex/program_committe.tex')
 	include_file('../%s.tex' % page, '../source/latex/awards.tex')
+	include_file('../%s.tex' % page, '../source/latex/patents.tex')
+	include_file('../%s.tex' % page, '../source/latex/teaching.tex')
+	include_file('../%s.tex' % page, '../source/latex/talks.tex')
 	include_file('../%s.tex' % page, '../source/latex/footer.tex')
