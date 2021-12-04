4cbc8cf25135acb86afd5f76ec3a13b0cb5951e1
Jaesik Park <syncle@gmail.com>
2021-12-04 18:11:30 +0900

    WIP

--- script/gen_headers.py
+++ script/gen_headers.py
@@ -9,15 +9,15 @@ def write_header_html(file, type):
 	active_contact = ''
 	active_lab = ''
 	if type == 'About me':
-		active_about_me = 'class="active"'
+		active_about_me = 'active'
 	elif type == 'Publications':
-		active_publications = 'class="active"'
+		active_publications = 'active'
 	elif type == 'Academic activities':
-		active_academic_activities = 'class="active"'
+		active_academic_activities = 'active'
 	elif type == '@POSTECH':
-		active_lab = 'class="active"'
+		active_lab = 'active'
 	elif type == 'Contact':
-		active_contact = 'class="active"'
+		active_contact = 'active'
 	elif type == "Index":
 		active_index = '' # do nothing
 	else:
@@ -26,88 +26,54 @@ def write_header_html(file, type):
 	header_str = ''
 	if type != 'Index':
 		header_str = \
-			'\n'+\
-			'<!-- Header -->\n'+\
-			'<header>\n'+\
-			'	<div class="container" id="maincontent" tabindex="-1">\n'+\
-			'		<div class="row">\n'+\
-			'			<div class="col-lg-12">\n'+\
-			'				<div class="intro-text">\n'+\
-			'					<h1>%s</h1>\n' % type +\
-			'				</div>\n'+\
-			'			</div>\n'+\
-			'		</div>\n'+\
+			'<header class="masthead bg-primary text-white test-start">\n'+\
+			'	<div class="container d-flex align-items-left flex-column">\n'+\
+			'		<h1 class="text-uppercase mb-0">%s</h1>\n' % type+\
 			'	</div>\n'+\
 			'</header>\n'+\
 			'\n'
 	if type == 'Publications':
 		header_str = \
-			'\n'+\
-			'<!-- Header -->\n'+\
-			'<header>\n'+\
-			'	<div class="container" id="maincontent" tabindex="-1">\n'+\
+			'<header class="masthead bg-primary text-white">\n'+\
+			'	<div class="container">\n'+\
 			'		<div class="row">\n'+\
-			'			<div class="col-lg-6 text-left">\n'+\
-			'				<div class="intro-text">\n'+\
-			'					<h1>%s</h1>\n' % type +\
-			'				</div>\n'+\
+			'			<div class="col-lg-6 text-start">\n'+\
+			'				<h1 class="text-uppercase mb-0">%s</h1>\n' % type +\
 			'			</div>\n'+\
-			'			<div class="col-lg-6 text-right">\n'+\
-			'				<div class="intro-text">\n'+\
-			'					<br>\n'+\
-			'					<a href="https://scholar.google.com/citations?user=_3q6KBIAAAAJ&hl=en" target="_blank" class="btn btn-default" role="button">\n' +\
-			'						<span class="glyphicon glyphicon-search"></span> Google Scholar\n'+\
-			'					</a>\n'+\
-			'					<a href="http://dblp.uni-trier.de/pers/hd/p/Park:Jaesik" target="_blank" class="btn btn-default" role="button">\n'+\
-			'						<span class="glyphicon glyphicon-search"></span> DBLP\n'+\
-			'					</a>\n'+\
-			'				</div>\n'+\
+			'			<div class="col-lg-6 text-end">\n'+\
+			'				<a href="https://scholar.google.com/citations?user=_3q6KBIAAAAJ&hl=en" target="_blank" class="btn btn-default" role="button">\n' +\
+			'					<span class="glyphicon glyphicon-search"></span> Google Scholar\n'+\
+			'				</a>\n'+\
+			'				<a href="http://dblp.uni-trier.de/pers/hd/p/Park:Jaesik" target="_blank" class="btn btn-default" role="button">\n'+\
+			'					<span class="glyphicon glyphicon-search"></span> DBLP\n'+\
+			'				</a>\n'+\
 			'			</div>\n'+\
 			'		</div>\n'+\
 			'	</div>\n'+\
 			'</header>\n'+\
-			'\n'
+			'\n'		
 
 	f = open(file, "w")
 	f.write(
-		'\n'+\
-		'<!-- Navigation -->\n'+
-		'<nav id="mainNav" class="navbar navbar-default navbar-fixed-top navbar-custom">\n'+
-		'	<div class="container">\n'+
-		'		<!-- Brand and toggle get grouped for better mobile display -->\n'+
-		'		<div class="navbar-header page-scroll">\n'+
-		'			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">\n'+
-		'				<span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>\n'+
-		'			</button>\n'+
-		'			<a class="navbar-brand" href="%s">Jaesik Park</a>\n' % get_domain()+
-		'		</div>\n'+
-		'		<!-- Collect the nav links, forms, and other content for toggling -->\n'+
-		'		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">\n'+
-		'			<ul class="nav navbar-nav navbar-right">\n'+
-		'				<li class="hidden">\n'+
-		'					<a href="%s"></a>\n' % get_domain()+
-		'				</li>\n'+
-		'				<li %s>\n' % active_about_me +
-		'					<a href="%s/about_me">About me</a>\n' % get_domain()+
-		'				</li>\n'+
-		'				<li %s>\n' % active_academic_activities +
-		'					<a href="%s/academic_activities">Academic activities</a>\n' % get_domain()+
-		'				</li>\n'+
-		'				<li %s>\n' % active_publications +
-		'					<a href="%s/publications">Publications</a>\n' % get_domain()+
-		'				</li>\n'+
-		'				<li %s>\n' % active_lab +
-		'					<a href="%s/lab">@POSTECH</a>\n' % get_domain()+
-		'				</li>\n'+
-		'				<li %s>\n' % active_contact +
-		'					<a href="%s/contact">Contact</a>\n' % get_domain()+
-		'				</li>\n'+
-		'			</ul>\n'+
-		'		</div>\n'+
-		'		<!-- /.navbar-collapse -->\n'+
-		'	</div>\n'+
-		'<!-- /.container-fluid -->\n'+
-		'</nav>\n'+
+		'<nav class="navbar navbar-expand-lg bg-secondary fixed-top text-uppercase" id="mainNav">\n' +
+		'	<div class="container">\n' +
+		'		<a class="navbar-brand" href="%s">Jaesik Park</a>\n' % get_domain()+
+		'		<button class="navbar-toggler font-weight-bold bg-primary text-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">\n' +
+		'			Menu\n' +
+		'			<i class="fas fa-bars"></i>\n' +
+		'		</button>\n' +
+		'		<div class="collapse navbar-collapse" id="navbarResponsive">\n' +
+		'			<ul class="navbar-nav ms-auto">\n' +
+		'				<li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3" href="%s"></a></li>\n' % get_domain()+
+		'				<li class="nav-item mx-0 mx-lg-1"><a class="nav-link %s py-3 px-0 px-lg-3" href="%s/about_me">About me</a></li>\n' % (active_about_me, get_domain())+
+		'				<li class="nav-item mx-0 mx-lg-1"><a class="nav-link %s py-3 px-0 px-lg-3" href="%s/academic_activities">Academic activities</a></li>\n' % (active_academic_activities, get_domain())+
+		'				<li class="nav-item mx-0 mx-lg-1"><a class="nav-link %s py-3 px-0 px-lg-3" href="%s/publications">Publications</a></li>\n' % (active_publications, get_domain())+
+		'				<li class="nav-item mx-0 mx-lg-1"><a class="nav-link %s py-3 px-0 px-lg-3" href="%s/lab">@POSTECH</a></li>\n' % (active_lab, get_domain())+
+		'				<li class="nav-item mx-0 mx-lg-1"><a class="nav-link %s py-3 px-0 px-lg-3" href="%s/contact">Contact</a></li>\n' % (active_contact, get_domain())+
+		'			</ul>\n' +
+		'		</div>\n' +
+		'	</div>\n' +
+		'</nav>\n' +
 		'\n'+
 		header_str +
 		'\n')
