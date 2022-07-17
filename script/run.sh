f70f3fcaee48aefe1a56c6b217257f2d2c8efb76
syncle <syncle@gmail.com>
2022-07-17 16:03:14 +0900

    latex folder structure

--- script/run.sh
+++ script/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# bash function for checking return value of the script
+### bash function for checking return value of the script
 run_and_check_success()
 {
 	$1					# run script
@@ -12,12 +12,19 @@ run_and_check_success()
 	fi
 }
 
+### prepare images for the publication page
+run_and_check_success "python resize_images.py"
+
+### make html files
+run_and_check_success "python gen_index.py"
 run_and_check_success "python gen_publication_html.py"
 run_and_check_success "python gen_button_for_project_page.py"
 run_and_check_success "python gen_headers.py"
 run_and_check_success "python assemble_files.py"
-run_and_check_success "python resize_images.py"
+
+### build latex cv.pdf
+run_and_check_success "mkdir ../source/latex/build"
 run_and_check_success "python gen_latex.py"
-run_and_check_success "pdflatex --interaction=batchmode --jobname=cv -output-directory .. ../cv.tex 2>&1 > /dev/null"
-run_and_check_success "gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.5 -dNOPAUSE -dQUIET -dBATCH -dPrinted=false -sOutputFile=../cv_jaesik_park.pdf ../cv.pdf"
-run_and_check_success "rm ../cv.*"
+run_and_check_success "pdflatex --interaction=batchmode --jobname=cv -output-directory ../source/latex/build ../source/latex/cv.tex 2>&1 > /dev/null"
+### compress cv.pdf (1/10 smaller)
+run_and_check_success "gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.5 -dNOPAUSE -dQUIET -dBATCH -dPrinted=false -sOutputFile=../cv_jaesik_park.pdf ../source/latex/build/cv.pdf"
