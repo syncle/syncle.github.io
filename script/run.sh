c1e0aaed953b18c123314e22295ca19e57a42756
syncle <syncle@gmail.com>
2022-07-18 13:54:53 +0900

    remove unnecessary latex files

--- script/run.sh
+++ script/run.sh
@@ -23,8 +23,8 @@ run_and_check_success "python gen_headers.py"
 run_and_check_success "python assemble_files.py"
 
 ### build latex cv.pdf
-run_and_check_success "mkdir ../source/latex/build"
 run_and_check_success "python gen_latex.py"
 run_and_check_success "pdflatex --interaction=batchmode --jobname=cv -output-directory ../source/latex/build ../source/latex/cv.tex 2>&1 > /dev/null"
 ### compress cv.pdf (1/10 smaller)
 run_and_check_success "gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.5 -dNOPAUSE -dQUIET -dBATCH -dPrinted=false -sOutputFile=../cv_jaesik_park.pdf ../source/latex/build/cv.pdf"
+run_and_check_success "rm ../source/latex/cv.tex"
