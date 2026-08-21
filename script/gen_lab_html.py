43097ffd63aa8a602eb264c9d2971dce8e22cb00
Jaesik Park <syncle@gmail.com>
2026-08-21 10:44:20 +0900

    update lab page

--- script/gen_lab_html.py
+++ script/gen_lab_html.py
@@ -1,7 +1,7 @@
 import json
 from util import dump_str_to_html_placeholder
 
-def dump_other_info(i, name):
+def dump_member_info(i, name):
 	temp = '%s' % ('			<h5><a href="%s" target="_blank">%s</a></h5>\n' % (i['web'], name) if 'web' in i else '			<h5>%s</h5>\n' % name) + \
 				'%s' % ('			' + i['ms_start'] if 'ms_start' in i else '') + \
 				'%s' % (' - ' + i['ms_end'] + '<br>\n' if 'ms_end' in i else '') + \
@@ -18,31 +18,40 @@ def dump_other_info(i, name):
 				'		</div>\n'
 	return temp
 
-def write_members(input_path, output_path, data):
+def dump_hardware_info(i, name):
+	temp = '%s' % ('			<h5><a href="%s" target="_blank">%s</a></h5>\n' % (i['web'], name) if 'web' in i else '			<h5>%s</h5>\n' % name) + \
+				'%s' % ('			' + i['comment'] + '<br>\n' if 'comment' in i else '') + \
+				'		</div>\n'
+	return temp
+
+def write_members(input_path, output_path, data, member_now='yes', autogen='AUTOGEN::Members'):
 	dump_str = []
 	web = []
 	for idx, name in reversed(list(enumerate(data))):
 		i = data[name]
-		if ('member_now' in i and i['member_now'] == 'yes'):
+		if ('member_now' in i and i['member_now'] == member_now):
 			dump_str.append('		<div class="col-md-2 text-center">\n' + 
 							'			<img src="%s" class="rounded-circle img-thumbnail shadow mb-2 d-none d-md-block">\n' % i['photo'])
-			dump_str.append(dump_other_info(i, name))
-	dump_str_to_html_placeholder(input_path, output_path, 'AUTOGEN::Members', dump_str)
+			dump_str.append(dump_member_info(i, name))
+	dump_str_to_html_placeholder(input_path, output_path, autogen, dump_str)
 
-def write_past_members(input_path, output_path, data):
+def write_hardwares(input_path, output_path, data):
 	dump_str = []
 	web = []
-	for idx, name in enumerate(data):
+	for idx, name in list(enumerate(data)):
 		i = data[name]
-		if ('member_now' not in i or i['member_now'] != 'yes'):
+		if ('available_now' in i and i['available_now'] == 'yes'):
 			dump_str.append('		<div class="col-md-2 text-center">\n' + 
 							'			<img src="%s" class="rounded-circle img-thumbnail shadow mb-2 d-none d-md-block">\n' % i['photo'])
-			dump_str.append(dump_other_info(i, name))
-	dump_str_to_html_placeholder(input_path, output_path, 'AUTOGEN::PastMembers', dump_str)
+			dump_str.append(dump_hardware_info(i, name))
+	dump_str_to_html_placeholder(input_path, output_path, 'AUTOGEN::Hardwares', dump_str)
 
 if __name__ == "__main__":
 	with open('../data/students.json') as f:
-		data = json.load(f)
+		data_students = json.load(f)
+	with open('../data/hardwares.json') as f:
+		data_hardwares = json.load(f)
 	
-	write_members('../source/html/lab_skeleton.html', '../source/html/lab.html', data)
-	write_past_members('../source/html/lab.html', '../source/html/lab.html', data)
+	write_members('../source/html/lab_skeleton.html', '../source/html/lab.html', data_students)
+	write_hardwares('../source/html/lab.html', '../source/html/lab.html', data_hardwares)
+	write_members('../source/html/lab.html', '../source/html/lab.html', data_students, 'no', 'AUTOGEN::PastMembers')
